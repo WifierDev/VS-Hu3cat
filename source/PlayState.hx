567f0f0cc12f8a8f1f8648e1332436ede9b8e6c4
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2022-05-11 17:56:36 -0300

    xd

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,4 +1,4 @@
-package;
+package; // agora quem esta no comando é eu ALY ANT HAHAHAHAHA
 
 #if desktop
 import Discord.DiscordClient;
@@ -66,16 +66,16 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
+		['go play tutorial', 0.2], //From 0% to 19%
+		['xd', 0.4], //From 20% to 39%
+		['dumb lmao', 0.5], //From 40% to 49%
 		['Bruh', 0.6], //From 50% to 59%
 		['Meh', 0.69], //From 60% to 68%
 		['Nice', 0.7], //69%
 		['Good', 0.8], //From 70% to 79%
 		['Great', 0.9], //From 80% to 89%
 		['Sick!', 1], //From 90% to 99%
-		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
+		['bitch good', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 	
 	public var modchartTweens:Map<String, FlxTween> = new Map<String, FlxTween>();
@@ -152,6 +152,10 @@ class PlayState extends MusicBeatState
 	private var curSong:String = "";
 	
 	public static var segredoDoido:Bool = false;
+
+	var bfDodging:Bool = false; //mecânica do dodge xd foi o pietro que pediu lol
+	var bfCanDodge:Bool = false;
+	var alert:FlxSprite;
 
 	public var gfSpeed:Int = 1;
 	public var health:Float = 1;
